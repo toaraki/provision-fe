8283e3ed51c8da994d95abe3072ef72549a592c2
Toshihiro Araki <toaraki@redhat.com>
2025-09-24 09:12:49 +0900

    カスタムイメージのリトライ

--- app.py
+++ app.py
@@ -44,14 +44,13 @@ def deploy():
                     "containers": [
                         {
                             "name": "deployer-job",
-                            "image": "registry.redhat.io/openshift4/ose-cli:latest",
+                            # "image": "registry.redhat.io/openshift4/ose-cli:latest",
+                            "image": "image-registry.openshift-image-registry.svc:5000/user20-vm-iac/job-runner-image:latest",
                             "command": ["/bin/bash", "-c"],
                             "args": [
                                 f"set -e; "
                                 f"VM_NAME='{normalized_hostname}'; "
                                 f"TEMPLATE_URL='https://raw.githubusercontent.com/toaraki/vm-templates/main/vm-fedora-template.yaml'; "
-                                f"export PATH=/usr/local/bin:$PATH; "
-                                f"ls /usr/local/bin; "
                                 f"curl -s -k -L $TEMPLATE_URL | sed 's/{{{{ .hostname }}}}/{normalized_hostname}/g' | oc apply -f -; "
                                 f"echo 'Waiting for VM to be ready...'; "
                                 f"oc wait --for=condition=ready --timeout=300s vm/$VM_NAME; "
